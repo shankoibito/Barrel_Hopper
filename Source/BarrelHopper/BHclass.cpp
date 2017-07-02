52175d120a6f1b974eff04312766116a019b72dc
Shanbir Singh <shanbirsingh@gmail.com>
2017-07-03 03:15:57 +0530

    I commit

---
+++ Source/BarrelHopper/BHclass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BarrelHopper.h"
+#include "BHclass.h"
+
+BHclass::BHclass()
+{
+}
+
+BHclass::~BHclass()
+{
+}
---
+++ Source/BarrelHopper/BarrelHopper.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BarrelHopper.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BarrelHopper, "BarrelHopper" );
